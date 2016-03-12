f81e3f5bc37b54449662b4e0cae49510f8d42270
qjx <qjx>
2016-03-12 21:40:22 +0800

    创建了person类

---
+++ qq/qq/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  qq
+//
+//  Created by 权建星 on 16/3/12.
+//  Copyright © 2016年 权建星. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ qq/qq/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  qq
+//
+//  Created by 权建星 on 16/3/12.
+//  Copyright © 2016年 权建星. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
